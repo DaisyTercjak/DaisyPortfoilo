7ae3bb2b5d67668ed412a0bc67697a4e54907552
DaisyTercjak <daisy.schoolhw@gmail.com>
2025-06-30 22:37:20 +0100

    further corrections

--- js/auth.js
+++ js/auth.js
@@ -39,7 +39,7 @@ function updateNavigation(isAuthenticated) {
     } else {
         navItems.innerHTML = `
             <li class="nav-item"><a class="nav-link" href="pages/signup.html">Sign Up</a></li>
-            <li class="nav-item"><a class="nav-link" href="pages/login.html">Login</a></li>
+            
         `;
     }
 }
